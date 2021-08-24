7754f16b88c7534ca0d7a4cacd83f7a68f9effcd
Lawrence Lockhart <ldlockhart@gmail.com>
2021-08-24 06:32:18 -0500

    idontevenknow

--- Strings.java
+++ Strings.java
@@ -6,6 +6,9 @@ public class Strings{
 
         String greeting = "hello";
         String name = "Lawrence";
+        char[] nameChars = name.toCharArray();
+        System.out.println(nameChars);
+        System.out.println("greeting and name are the same or not: "  + (greeting == name));
         String together = greeting + " " + name;
         String smashed = greeting.concat(name);
         System.out.println(greeting + " and " + together);
@@ -21,5 +24,16 @@ public class Strings{
         System.out.println("Input a letter");
         string1 = scn.nextLine();
         System.out.println(string1);
+        System.out.println("This is a substring of Lawrence: " + name.substring(2,3));
+        System.out.println("----------------------------------");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i=0; i<10; i++) {
+            stringBuilder.append("*");
+        }
+        System.out.println(stringBuilder);
+        String a = "11";
+        String b = "111";
+        System.out.println("String a is " + a + " and a minus 1 is " + (a.charAt(0)-'1'));
+        System.out.println((5<3) ? "no way" : "yep that's right");
     }
 }
